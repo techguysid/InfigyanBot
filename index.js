aabcd1d9f67885e5cdd6752672bc0f7b06336511
Siddharth Udeniya <thundercut@Siddharths-MacBook-Pro.local>
2019-04-06 04:18:03 +0530

    webhook-update

--- index.js
+++ index.js
@@ -11,23 +11,6 @@ const botPort = process.env.PORT||2000
 bot.telegram.setWebhook(process.env.URL)
 bot.telegram.startWebhook(process.env.STARTWEBHOOK,null,botPort)
 
-
-expressApp.listen(process.env.port||3000, () => {
-  console.log('Example app listening on port 3000!')
-})
-
-expressApp.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
-  expressApp.get('/launchBot', (req, res) => {
-    res.send('Hello World!')
-  })
-
-  expressApp.post('/launchBot', (req, res) => {
-    res.send('Hello World!')
-  })
-
   bot.start((ctx) => ctx.reply('Welcome'))
   bot.help((ctx) => ctx.reply('Send me a sticker'))
   bot.on('sticker', (ctx) => ctx.reply('👍'))
