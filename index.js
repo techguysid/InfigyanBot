b9dafa50b5d4b6b226fe2c7e723f146434924304
Siddharth Udeniya <thundercut@Siddharths-MacBook-Pro.local>
2019-04-06 02:46:27 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+require("dotenv").config();
+
+const Telegraf = require('telegraf')
+const express = require('express')
+const expressApp = express()
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+expressApp.use(bot.webhookCallback('/'))
+bot.telegram.setWebhook('https://server.tld:8443/secret-path')
+
+
+
+expressApp.listen(process.env.port||3000, () => {
+  console.log('Example app listening on port 3000!')
+})
+
+expressApp.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  bot.start((ctx) => ctx.reply('Welcome'))
+  bot.help((ctx) => ctx.reply('Send me a sticker'))
+  bot.on('sticker', (ctx) => ctx.reply('👍'))
+  bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+  bot.launch()
