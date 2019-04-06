3886ae1ff3eb8f027f3d8d761a0b08dbc7dd7637
Siddharth Udeniya <thundercut@Siddharths-MacBook-Pro.local>
2019-04-06 16:56:49 +0530

    Added methods

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
     
 const Telegraf = require("telegraf"); // import telegraf lib
+const Markup = require("telegraf/markup");
 const BOT_TOKEN = process.env.BOT_TOKEN
 const PORT = process.env.PORT||2000
 console.log(BOT_TOKEN)
@@ -9,7 +10,17 @@ bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey '+
 ctx.from.first_name+'! How may I help you today?\n You can give me following commands:\n'+
-'1. /sports\-awards - To help you with the list of awardees in the field of sports\n'+
-'2. /literature\-awards - To help you with list of awardees in the field of literature.'))
+'1. /awards/sports - To help you with the list of awardeesin the field of sports\n'+
+'2. /awards/literature - To help you with list of awardees in the field of literature.'+
+'3. /awards/nationalhonors - To help you with the list of receipents of Indian National Honorsa like Bharat Ratna etc.\n'+
+'4. /awards/music - To help you with list of awardees in the field of literature.\n'+
+'5. /internationalOrganizations - To help you with the list of all the international organizations,their HQs and members\n'+
+'6. /importantDaysOfTheCalender - List of all days of national or international importance'))
 bot.launch()
-bot.command('sports',(ctx) => ctx.reply('I can tell you about Arjuna Awards.'))
+bot.command('/awards/sports',(ctx) => ctx.reply('Please select the award category to proceed :\n'+
+'1. /awards/sports/dronacharya - List of Dronacharya Awardees\n'+
+'2. /awards/sports/rajivgandhikhelratna - List of Rajiv Gandhi Khel Ratna Awardees\n'+
+'3. /awards/sports/arjuna - List of Arjuna Awardees\n'+
+'4. /awards/sports/dhyanchand - List of Dhyan Chand Awardees'))
+
+bot.command('awards/sports/dronacharya',(ctx) => ctx.replyWithHTML('<b>Subedar C.A.Kuttappa</b>-<b>Boxing</b><i>Subedar ban ke boxing karne lage</i>\n'))
