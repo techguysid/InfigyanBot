2f4cc1a490976b10c420539f7094393da9cc2760
Siddharth Udeniya <thundercut@Siddharths-MacBook-Pro.local>
2019-04-06 11:27:11 +0530

    Added telegraf as dependency

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ const BOT_TOKEN = process.env.BOT_TOKEN
 const PORT = process.env.PORT||2000
 console.log(BOT_TOKEN)
 const bot = new Telegraf(process.env.BOT_TOKEN); // Get the token from the environment variable
-const URL = process.env.
-bot.telegram.setWebhook(`${URL}bot${BOT_TOKEN}`);
-bot.telegram.startWebhook(`/bot${BOT_TOKEN}`,null, PORT)
-// Start Bot
-bot.start(ctx => {
-  ctx.reply(
-    `How can I help you, ${ctx.from.first_name}?`
-  );
-});
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
