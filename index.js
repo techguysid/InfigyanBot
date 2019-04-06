eff1a8fa8838ac5ec5a63b9be4c93e3d1455ee25
Siddharth Udeniya <thundercut@Siddharths-MacBook-Pro.local>
2019-04-06 13:56:33 +0530

    Added /sports

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const bot = new Telegraf(process.env.BOT_TOKEN); // Get the token from the envir
 bot.start((ctx) => ctx.reply('Welcome '+ctx.from.first_name+'! Stay Motivated! Happy Learning.'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey '+ctx.from.first_name+'! How may I help you today?'))
+bot.hears('hi', (ctx) => ctx.reply('Hey '+
+ctx.from.first_name+'! How may I help you today?\n You can give me following commands:\n'+
+'1. /sports-awards - To help you with the list of awardees in the field of sports\n'+
+'2. /literature-awards - To help you with list of awardees in the field of literature.'))
 bot.launch()
 bot.command('sports',(ctx) => ctx.reply('I can tell you about Arjuna Awards.'))
