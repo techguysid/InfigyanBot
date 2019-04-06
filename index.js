38940781d3cac8ed55af4e45deeca1e8b353ffce
Siddharth Udeniya <thundercut@Siddharths-MacBook-Pro.local>
2019-04-06 13:58:22 +0530

    Added /sports

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey '+
 ctx.from.first_name+'! How may I help you today?\n You can give me following commands:\n'+
-'1. /sports-awards - To help you with the list of awardees in the field of sports\n'+
-'2. /literature-awards - To help you with list of awardees in the field of literature.'))
+'1. /sports\-awards - To help you with the list of awardees in the field of sports\n'+
+'2. /literature\-awards - To help you with list of awardees in the field of literature.'))
 bot.launch()
 bot.command('sports',(ctx) => ctx.reply('I can tell you about Arjuna Awards.'))
